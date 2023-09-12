c95a4521e44db3cf79b779c92f19eebe61b503fc
Ethan Lopez <ethanlopez2802@gmail.com>
2023-09-12 18:38:15 -0400

    Improved scatterplot performance
    drawing each canvas no longer requires a
    loop through each data point

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plot
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import tkinter as tk
 from tkinter import messagebox
+import numpy as np
+
 
 def on_closing():
     if messagebox.askokcancel("Quit Program", "Do you want to quit?"):
@@ -15,24 +17,27 @@ window.state('zoomed')  # full-screen
 window.protocol("WM_DELETE_WINDOW", on_closing)
 
 # initialize Tkinter
-figure, ax = plot.subplots()
+plot.xticks(np.arange(0, 5, 1))
+figure, (ax, ax2) = plot.subplots(1, 2, figsize=(10, 5))
 
 # Tkinter app
 frame = tk.Frame(window)
 label = tk.Label(text="Scatterplot Test")
 label.pack()
 
+# canvas initialize
 canvas = FigureCanvasTkAgg(figure, master=frame)
 canvas.get_tk_widget().pack()
 
 frame.pack()
 
+# data extraction
 gpas, hours = Util.get_data_info()
 gpas = Util.dict_to_pairs(gpas)
 hours = Util.dict_to_pairs(hours)
 
-for gpa in gpas:
-    ax.scatter(gpa[1], gpa[0])
-canvas.draw()
+# data visualization
+ax.plot(Util.get_xs(gpas), Util.get_ys(gpas), 'k.', ms=5)
+ax2.plot(Util.get_xs(hours), Util.get_ys(hours), 'k.', ms=5)
 
 window.mainloop()
