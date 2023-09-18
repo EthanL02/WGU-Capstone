fbaf296adbca11a6be5e35cbf4a66e961466f90f
Ethan Lopez <ethanlopez2802@gmail.com>
2023-09-18 12:31:38 -0400

    Suggest minimum hours for goal gpa
    user inputs desired gpa
    minimum hours per week is output on a label
    hours are estimated from regression line
    on the scatterplot

--- main.py
+++ main.py
@@ -34,6 +34,19 @@ def on_closing():
         window.quit()
 
 
+def on_gpa_entered(*args):
+    try:
+        gpa = float(gpa_text_var.get())
+        get_study_hours(gpa)
+        return True
+    except ValueError:
+        return False
+
+
+def get_study_hours(gpa):
+    gpa_label.config(text=f"Study hours for Gpa '{gpa}': {round(np.interp(gpa, ml_xdata, ml_ydata), 2)} hrs / week")
+
+
 # create window
 window = tk.Tk()
 window.state('zoomed')  # full-screen
@@ -50,15 +63,27 @@ frame = tk.Frame(window)
 Create_Graph.frame = frame
 label = tk.Label(text="Scatterplot Test")
 label.pack()
+
 # dynamic ui
+# pie chart combo box
 pie_cbox = ttk.Combobox(window, values=Util.get_dict_keys(data_dict), state="readonly")
 pie_cbox.pack(side="top", anchor="e")
 pie_cbox.bind("<<ComboboxSelected>>", on_pie_select)
 
+# gpa entry field
+gpa_text_var = tk.StringVar()
+gpa_entry = tk.Entry(window, textvariable=gpa_text_var)
+gpa_entry.pack(side="top", anchor="w")
+gpa_text_var.trace("w", on_gpa_entered)
+
+# gpa text field
+gpa_label = tk.Label(window, text="Study hours per week for Gpa '': ")
+gpa_label.pack(side="top", anchor="w")
+
 # data visualization
 scatter_graph, scatter_canvas = Create_Graph.scatter(Util.get_xs(data_pair), Util.get_ys(data_pair))
 ml_model = np.poly1d(np.polyfit(Util.get_xs(data_pair), Util.get_ys(data_pair), 3))
-ml_line = np.linspace(np.min(Util.get_xs(data_pair)), np.max(Util.get_xs(data_pair)), 100)
+ml_line = np.linspace(np.min(Util.get_xs(data_pair)), np.max(Util.get_xs(data_pair)), int(np.max(Util.get_ys(data_pair))))
 guess_line, = scatter_graph.plot(ml_line, ml_model(ml_line))
 ml_xdata = guess_line.get_xdata()
 ml_ydata = guess_line.get_ydata()
